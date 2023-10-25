b966505c650948e565b5a167911234b6607322c2
liruifengv <liruifeng1024@gmail.com>
2023-10-25 22:19:07 +0800

    fix: fix about page padding

--- layouts/AboutLayout.tsx
+++ layouts/AboutLayout.tsx
@@ -14,7 +14,7 @@ export default function AboutLayout({ children, content }: Props) {
 
   return (
     <div className="flex w-full flex-col px-5 md:w-3/5 md:flex-row">
-      <div className="md:3/4 mx-auto w-full divide-y divide-gray-200 px-4 dark:divide-gray-700 sm:px-6 xl:px-0">
+      <div className="md:3/4 divide-gray-200dark:divide-gray-700 mx-auto w-full divide-y">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             About
